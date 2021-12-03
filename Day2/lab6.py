c7a6d34af711b79ae5997226801280ee9fd37007
Abhyas <adhikari.abhyash@gmail.com>
2021-12-03 19:03:20 +0545

    Day2 - Corrected Programs

--- Day2/lab6.py
+++ Day2/lab6.py
@@ -2,3 +2,7 @@ a=(4/(25/60)+2*10)
 print("{} minutes for bus".format(a))
 b=(1/(7/60)+2/(15/60)+1/(7/60))
 print("{} minutes for jogging".format(b))
+if a<b:
+    print("Bus is quicker than running")
+else:
+    print("Running is quicker than bus")
